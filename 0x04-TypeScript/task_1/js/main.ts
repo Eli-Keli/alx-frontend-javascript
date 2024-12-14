b4a2c5726f2b5ed2e1a5b2a2ab2f9f7ab60643dd
Eli-Keli <muthokaelikeli@gmail.com>
2024-12-14 15:33:51 +0300

    Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -54,3 +54,34 @@ function printTeacher(firstName: string, lastName: string): string {
 }
 
 console.log(printTeacher('John', 'Doe')); // should print "J. Doe"
+
+
+
+interface StudentConstructor {
+    new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
