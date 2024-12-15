b00e673d0996c2b2569bad6484f44dfdeb17f755
Eli-Keli <muthokaelikeli@gmail.com>
2024-12-16 00:16:39 +0300

    Creating functions specific to employees

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -49,3 +49,19 @@ function createEmployee(salary: number | string): Director | Teacher {
 console.log(createEmployee(200));
 console.log(createEmployee(1000));
 console.log(createEmployee('500'));
+
+
+function isDirector(employee: Director | Teacher): employee is Director { // employee is Director is a type guard that checks if the employee is a Director
+    return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: Director | Teacher): string {
+    if (isDirector(employee)) {
+        return employee.workDirectorTasks();
+    } else {
+        return employee.workTeacherTasks();
+    }
+}
+
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
